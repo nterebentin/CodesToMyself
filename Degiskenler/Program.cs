6c65239a891e6408f6e91ebe60c4657787cccf2a
Nursen Ozcan <nursenozcan95@gmail.com>
2023-01-17 11:29:29 +0300

    Add project files.

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                       
+            int uzunkenar, kisakenar, alan, cevre;
+            uzunkenar = 15;
+            kisakenar = 20;
+            alan = uzunkenar * kisakenar;
+            cevre = 2 * (uzunkenar + kisakenar);
+            Console.WriteLine("Alan : "  +alan + " " + "Cevre : " +cevre);
+            Console.ReadLine();
+
+
+        }
+
+        
+    }
+
+}
